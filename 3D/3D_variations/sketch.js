2e40dd7fc74c700004f614b166a43003f69add6e
Kathy <65121394+kfahn22@users.noreply.github.com>
2023-06-15 09:07:52 -0400

    add some edits to sketch

--- 3D/3D_variations/sketch.js
+++ 3D/3D_variations/sketch.js
@@ -1,15 +1,33 @@
 // The spherical code is based on Daniel Shiffman's 3d-supershapes challenge
 // https://thecodingtrain.com/challenges/26-3d-supershapes
-// This is my best attempt at creating a 3D version
+// I don't know what to call this shape
 
+// Reference for hyperbolic functions
+// https://byjus.com/maths/hyperbolic-function/
+// https://help.tc2000.com/m/69445/l/755460-hyperbolic-functions-table
+
+
+// Variations:
+// a = 1, b = 4; sc = 2, sp = 4 yields a "xmas-tree" topper like shape
+// a + (1 / b) * hyperbolicSin(b * sin(sp * theta));
+// If sp = 8, 16 get some different fun shapes
+
+// a = 1, b = 4; sc = 80, sp = 4 yields a folded paper like shape
+// a + (1 / b) * hyperbolicTan(b * cos(sp * theta))
+
+// a = 1, b = 4, sc = 80, sp = 16 yields a folded structure with rounded edges
+// return a + (1 / b) * hyperbolicCot(b * cos(sp * theta));
+
+// adjusting total also yields some interesting results, but there is a 
+// limit to how much you can increase total before the animation slows way down
 let angX = 0;
 let angY = 0;
 let gears = [];
 let rotation = true;
 let total = 16;
 let num;
-let sc = 120;
-let sp = 4; // number of spokes
+let sc = 80;
+let sp = 32; // number of spokes
 
 function setup() {
   createCanvas(600, 600, WEBGL);
@@ -63,14 +81,36 @@ function hyperbolicTan(theta) {
   return (l - 1) / (l + 1);
 }
 
+function hyperbolicCot(theta) {
+  let e = 2.71828;
+  let k = pow(e, theta);
+  let l = pow(e, -theta);
+  return (k + l) / (k - l);
+}
+
+function hyperbolicSin(theta) {
+  let e = 2.71828;
+  let k = pow(e, theta);
+  let l = pow(e, -theta);
+  return (k - l) / 2;
+}
+function hyperbolicCos(theta) {
+  let e = 2.71828;
+  let k = pow(e, theta);
+  let l = pow(e, -theta);
+  return (k + l) / 2;
+}
+
 // Function to calculate r1, r2
 function gear(theta) {
   let a = 1;
   let b = 4; // changing this value yields a very different shape; b = 6
 
   // Equation for the radius of the gear curve
-  //return a + (1 / b) * hyperbolicTan(b * sin(sp * theta));
-  return a + (1 / b) * hyperbolicTan(b * cos(sp * theta));
+
+  //return a + (1 / b) * hyperbolicTan(b * cos(sp * theta));
+  //return a + (1 / b) * hyperbolicSin(b * cos(sp * theta));
+  return a + (1 / b) * hyperbolicCot(b * cos(sp * theta));
 }
 
 function mousePressed() {
